df3b0f812f57b31a74d0577ea036eec6062355f0
naltukhov <naltukhov@ourgapps.com>
2022-09-01 22:09:32 +0300

    Add lemma to inspiration

--- test_rake.py
+++ test_rake.py
@@ -1,14 +1,23 @@
-from multi_rake import Rake
 import pandas as pd
 
 df = pd.read_csv('data/agg-setup-punch-dataset/agg-setup-punch-dataset-test.csv')
 
+import spacy
+from multi_rake import Rake
 rake = Rake(language_code='ru')
+nlp = spacy.load("ru_core_news_md")
+def get_inspiration(text):
+    keywords = rake.apply(text)
+    if len(keywords) == 0:
+        return ''
+    keyword = keywords[0][0]
+    lem_keywords = [token.lemma_ for token in nlp(keyword)]
+    return ' '.join(lem_keywords)
+
 
 for _ in range(10):
 
     text = df.punch.sample().values[0]
     print('Input: ', text)
-
-    keywords = rake.apply(text)
-    print('Keywords: ', keywords[0])
+    inspiration = get_inspiration(text)
+    print('Inspiration: ', inspiration)
